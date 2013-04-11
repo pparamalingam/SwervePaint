dae9148373d74d6befe077bc7622d338afc0ac5e
Preshoth Paramalingam <preshoth@gmail.com>
2013-04-11 05:31:54 -0400

    Group/Ungroup cleanup

--- src/commands/Ungroup.java
+++ src/commands/Ungroup.java
@@ -1,32 +1,26 @@
 package commands;
 
-import java.awt.Color;
-import java.util.ArrayList;
-
-import javax.swing.BorderFactory;
-
 import tools.Canvas;
-import tools.Shape;
 
 public class Ungroup implements Command {
- 
- Canvas _c, _oldC;
 
- 
- public Ungroup(Canvas curr, Canvas backup ){
-  _c = curr;
-  _oldC = backup;
- }
- 
- @Override
- public void execute() {
-  _c.ungroupThemShapes();
- }
+	Canvas _c, _oldC;
 
- @Override
- public void unexecute() {
-	//_c = _oldC; 
-	_c.groupThemShapes();
- }
+
+	public Ungroup(Canvas curr, Canvas backup ){
+		_c = curr;
+		_oldC = backup;
+	}
+
+	@Override
+	public void execute() {
+		_c.ungroupThemShapes();
+	}
+
+	@Override
+	public void unexecute() {
+		//_c = _oldC; 
+		_c.groupThemShapes();
+	}
 
 }
